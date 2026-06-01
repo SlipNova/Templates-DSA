717d5295a4fb3652701368ea8de642db66e5821a
Sam <sam@Samarths-MacBook-Air.local>
2026-06-01 10:45:38 -0700

    update

--- ini.cpp
+++ ini.cpp
@@ -5,7 +5,10 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 using unit = unsigned int;
+<<<<<<< HEAD
 using ull = unsigned long long;
+=======
+>>>>>>> b6caff5 (update)
 
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()

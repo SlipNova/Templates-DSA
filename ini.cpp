07d1fc9a63ebd3629feda4fb8ab9589e8c8a635a
Sam <sam@Samarths-MacBook-Air.local>
2026-06-01 10:45:38 -0700

    update

--- ini.cpp
+++ ini.cpp
@@ -1,32 +1,35 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <map>
-#include <set>
-#include <unordered_set>
-#include <unordered_map>
-#include <cmath>
-#include <climits>
-#include <numeric>
-#include <iomanip>
-#include <stdexcept>
-
-typedef long long ll;
-typedef long double ld;
-
-const int MOD = 1e9 + 7;
+#include <bits/stdc++.h>
 
 using namespace std;
+
+using ll = long long;
+using ld = long double;
+using unit = unsigned int;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int INF = 1e9;
+const ll LINF = 4e18;
+const int MOD = 1e9 + 7;
+const ld PI = acos((ld) - 1);
+
+struct PairHash {
+    size_t operator()(const pair<int,int>& p) const {
+        return hash<ll>()(((ll)p.first << 32) ^ p.second);
+    }
+};
+
 
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        
+    }
     return 0;
 }
 
